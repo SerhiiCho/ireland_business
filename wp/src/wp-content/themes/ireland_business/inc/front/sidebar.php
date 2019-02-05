6a28ba32ac3bc0e10754600bd4f1c417579b3403
SerhiiCho <serhiicho@inbox.ru>
2019-02-05 14:06:04 +0200

    -1- Added navbar with items

---
+++ wp/src/wp-content/themes/ireland_business/inc/front/sidebar.php
@@ -0,0 +1,27 @@
+<!-- .ieb-sidebar -->
+<div class="ieb-sidebar">
+    <div class="ieb-sidebar__border"></div>
+    <div class="ieb-sidebar__border-thin"></div>
+
+    <div class="ieb-sidebar__bg">
+        <!-- .ieb-sidebar--table -->
+        <div class="ieb-sidebar__bg__table">
+            <div class="ieb-sidebar__bg__table__cell">
+                <!-- .ieb-sidebar__content -->
+                <div class="ieb-sidebar__bg__table__cell__content">
+                    <div class="ieb-sidebar__bg__table__cell__content__stars"></div>
+
+                    <h1 class="ieb-sidebar__bg__table__cell__content__title1">
+                        <?php _e('Genuinely Irish', 'ieb'); ?>
+                    </h1>
+                    <h2 class="ieb-sidebar__bg__table__cell__content__title2">
+                        <?php _e('EST. 1893', 'ieb'); ?>
+                    </h2>
+                </div>
+                <!-- / .ieb-sidebar__content -->
+            </div>
+        </div>
+        <!-- / .ieb-sidebar--table -->
+    </div>
+</div>
+<!-- / .ieb-sidebar -->
--- wp/src/wp-content/themes/ireland_business/index.php
+++ wp/src/wp-content/themes/ireland_business/index.php
@@ -1,36 +1,39 @@
 <?php wp_head(); ?>
 
-<!-- .ieb-sidebar -->
-<div class="ieb-sidebar">
-    <div class="ieb-sidebar__border"></div>
-    <div class="ieb-sidebar__border-thin"></div>
-
-    <div class="ieb-sidebar__bg">
-        <!-- .ieb-sidebar--table -->
-        <div class="ieb-sidebar__bg__table">
-            <div class="ieb-sidebar__bg__table__cell">
-                <!-- .ieb-sidebar__content -->
-                <div class="ieb-sidebar__bg__table__cell__content">
-                    <div class="ieb-sidebar__bg__table__cell__content__stars"></div>
-
-                    <h1 class="ieb-sidebar__bg__table__cell__content__title1">
-                        <?php _e('Genuinely Irish', 'ieb'); ?>
-                    </h1>
-                    <h2 class="ieb-sidebar__bg__table__cell__content__title2">
-                        <?php _e('EST. 1893', 'ieb'); ?>
-                    </h2>
-                </div>
-                <!-- / .ieb-sidebar__content -->
-            </div>
-        </div>
-        <!-- / .ieb-sidebar--table -->
-    </div>
-
-</div>
-<!-- / .ieb-sidebar -->
+<?php get_template_part('inc/front/sidebar'); ?>
 
 <!-- .ieb-wrapper -->
-<div class="ieb-wrapper"></div>
+<div class="ieb-wrapper">
+
+    <!-- .ieb-nav-wrap .ieb-nav-half -->
+    <div class="ieb-nav-wrap ieb-nav-half">
+        <nav class="ieb-nav-wrap__main">
+            <ul class="ieb-nav-wrap__main__menu">
+                <li class="ieb-nav-wrap__main__menu__item">
+                    <a href="#!" class="ieb-active"><?php _e('Главная', 'ieb'); ?></a>
+                </li>
+                <li class="ieb-nav-wrap__main__menu__item">
+                    <a href="#!"><?php _e('О нас', 'ieb'); ?></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <!-- / .ieb-nav-wrap / .ieb-nav-half -->
+
+    <!-- .ieb-wrapper__content -->
+    <div class="ieb-wrapper__content ieb-wrapper__content--in">
+        <div class="ieb-wrapper-inner">
+            <!-- .ieb-header -->
+            <header class="ieb-header">
+                <figure class="ieb-header__figure">
+                    <img src="http://demo.virtuti.info/demonstrations/vt12/wp-content/uploads/2016/02/scene.jpg" alt="">
+                </figure>
+            </header>
+            <!-- / .ieb-header -->
+        </div>
+    </div>
+    <!-- / .ieb-wrapper__content -->
+</div>
 <!-- / .ieb-wrapper -->
 
 <?php wp_footer(); ?>
