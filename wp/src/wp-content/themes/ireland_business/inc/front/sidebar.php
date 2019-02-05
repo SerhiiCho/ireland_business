87592b6fd5ee9369f43140ae93f88c24941ad5a9
SerhiiCho <serhiicho@inbox.ru>
2019-02-05 14:32:19 +0200

    -1- Added variables to _variables.sass

--- wp/src/wp-content/themes/ireland_business/inc/front/sidebar.php
+++ wp/src/wp-content/themes/ireland_business/inc/front/sidebar.php
@@ -1,13 +1,10 @@
-<!-- .ieb-sidebar -->
 <div class="ieb-sidebar">
     <div class="ieb-sidebar__border"></div>
     <div class="ieb-sidebar__border-thin"></div>
 
     <div class="ieb-sidebar__bg">
-        <!-- .ieb-sidebar--table -->
         <div class="ieb-sidebar__bg__table">
             <div class="ieb-sidebar__bg__table__cell">
-                <!-- .ieb-sidebar__content -->
                 <div class="ieb-sidebar__bg__table__cell__content">
                     <div class="ieb-sidebar__bg__table__cell__content__stars"></div>
 
@@ -17,11 +14,9 @@
                     <h2 class="ieb-sidebar__bg__table__cell__content__title2">
                         <?php _e('EST. 1893', 'ieb'); ?>
                     </h2>
-                </div>
-                <!-- / .ieb-sidebar__content -->
-            </div>
-        </div>
-        <!-- / .ieb-sidebar--table -->
-    </div>
-</div>
-<!-- / .ieb-sidebar -->
+
+                </div> <!-- / .ieb-sidebar__content -->
+            </div> <!-- / .ieb-sidebar__bg__table__cell -->
+        </div>  <!-- / .ieb-sidebar--table -->
+    </div> <!-- / .ieb-sidebar__bg -->
+</div> <!-- / .ieb-sidebar -->
--- wp/src/wp-content/themes/ireland_business/index.php
+++ wp/src/wp-content/themes/ireland_business/index.php
@@ -2,38 +2,30 @@
 
 <?php get_template_part('inc/front/sidebar'); ?>
 
-<!-- .ieb-wrapper -->
 <div class="ieb-wrapper">
+    <div class="ieb-wrapper-inner">
 
-    <!-- .ieb-nav-wrap .ieb-nav-half -->
-    <div class="ieb-nav-wrap ieb-nav-half">
-        <nav class="ieb-nav-wrap__main">
-            <ul class="ieb-nav-wrap__main__menu">
-                <li class="ieb-nav-wrap__main__menu__item">
-                    <a href="#!" class="ieb-active"><?php _e('Главная', 'ieb'); ?></a>
-                </li>
-                <li class="ieb-nav-wrap__main__menu__item">
-                    <a href="#!"><?php _e('О нас', 'ieb'); ?></a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    <!-- / .ieb-nav-wrap / .ieb-nav-half -->
+        <div class="ieb-nav-wrap ieb-nav-half">
+            <nav class="ieb-nav-wrap__main">
+                <ul class="ieb-nav-wrap__main__menu">
+                    <li class="ieb-nav-wrap__main__menu__item">
+                        <a href="#!" class="ieb-active"><?php _e('Главная', 'ieb'); ?></a>
+                    </li>
+                    <li class="ieb-nav-wrap__main__menu__item">
+                        <a href="#!"><?php _e('О нас', 'ieb'); ?></a>
+                    </li>
+                </ul>
+            </nav>
+        </div> <!-- / .ieb-nav-wrap --> <!-- / .ieb-nav-half -->
 
-    <!-- .ieb-wrapper__content -->
-    <div class="ieb-wrapper__content ieb-wrapper__content--in">
-        <div class="ieb-wrapper-inner">
-            <!-- .ieb-header -->
+        <div class="ieb-wrapper__content ieb-wrapper__content--in">
             <header class="ieb-header">
                 <figure class="ieb-header__figure">
                     <img src="http://demo.virtuti.info/demonstrations/vt12/wp-content/uploads/2016/02/scene.jpg" alt="">
                 </figure>
-            </header>
-            <!-- / .ieb-header -->
-        </div>
-    </div>
-    <!-- / .ieb-wrapper__content -->
-</div>
-<!-- / .ieb-wrapper -->
+            </header> <!-- / .ieb-header -->
+        </div> <!-- / .ieb-wrapper__content -->
+    </div> <!-- / .ieb-wrapper-inner -->
+</div> <!-- / .ieb-wrapper -->
 
 <?php wp_footer(); ?>
