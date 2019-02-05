ee17e71666d90d2d1df8db1bea7bd91f1e9abca3
SerhiiCho <serhiicho@inbox.ru>
2019-02-05 10:58:48 +0200

    -1- Added _wrapper.sass and changed _sidebar.sass Also added stars and fixed everything

--- wp/src/wp-content/themes/ireland_business/index.php
+++ wp/src/wp-content/themes/ireland_business/index.php
@@ -4,8 +4,16 @@
 <div class="ieb-sidebar">
     <div class="ieb-sidebar__border"></div>
     <div class="ieb-sidebar__border-thin"></div>
-
     <div class="ieb-sidebar__img"></div>
+
+    <div class="ieb-sidebar--table">
+        <div class="ieb-sidebar--align-middle">
+            <div class="ieb-sidebar__content">
+                <div class="ieb-sidebar__content__line"></div>
+            </div>
+        </div>
+    </div>
+
 </div>
 <!-- / .ieb-sidebar -->
 
