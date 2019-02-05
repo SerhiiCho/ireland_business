431f887e9a0ca9ba25b72bac982b8bc7ac3213e1
SerhiiCho <serhiicho@inbox.ru>
2019-02-05 16:52:35 +0200

    -1- Replaced linked images with real files from assets/img

---
+++ wp/src/wp-content/themes/ireland_business/inc/front/navmenu.php
@@ -0,0 +1,31 @@
+<!-- Desktop navmenu -->
+<div class="ieb-nav-wrap1 ieb-nav-half">
+    <nav class="ieb-nav-wrap1__main">
+        <ul class="ieb-nav-wrap1__main__menu">
+            <li class="ieb-nav-wrap1__main__menu__item">
+                <a href="#!" class="ieb-active"><?php _e('Главная', 'ieb'); ?></a>
+            </li>
+            <li class="ieb-nav-wrap1__main__menu__item">
+                <a href="#!"><?php _e('О нас', 'ieb'); ?></a>
+            </li>
+        </ul> <!-- / .ieb-nav-wrap1__main__menu -->
+    </nav> <!-- / .ieb-nav-wrap1__main -->
+</div> <!-- / .ieb-nav-wrap1 --> <!-- / .ieb-nav-half -->
+
+<!-- Mobile navmenu -->
+<div class="ieb-nav-wrap2 ieb-nav-half">
+    <nav class="ieb-nav-wrap2__mobile">
+        <!-- Show navmenu trigger -->
+        <a href="#!" class="ieb-nav-wrap2__mobile__ham">
+            <img src="<?php ieb_get_img('menu.png'); ?>" class="ieb-nav-wrap2__mobile__ham__img">
+        </a>
+        <ul class="ieb-nav-wrap2__menu">
+            <li class="ieb-nav-wrap2__menu__item">
+                <a href="#!"><?php _e('Главная', 'ieb'); ?></a>
+            </li>
+            <li class="ieb-nav-wrap2__menu__item">
+                <a href="#!"><?php _e('О нас', 'ieb'); ?></a>
+            </li>
+        </ul> <!-- ieb-nav-wrap2__menu -->
+    </nav> <!-- / .ieb-nav-wrap2__mobile -->
+</div> <!-- / .ieb-mobile-wrap2 --> <!-- / .ieb-nav-half -->
--- wp/src/wp-content/themes/ireland_business/index.php
+++ wp/src/wp-content/themes/ireland_business/index.php
@@ -5,39 +5,14 @@
 <div class="ieb-wrapper">
     <div class="ieb-wrapper-inner">
 
-        <div class="ieb-nav-wrap1 ieb-nav-half">
-            <nav class="ieb-nav-wrap1__main">
-                <ul class="ieb-nav-wrap1__main__menu">
-                    <li class="ieb-nav-wrap1__main__menu__item">
-                        <a href="#!" class="ieb-active"><?php _e('Главная', 'ieb'); ?></a>
-                    </li>
-                    <li class="ieb-nav-wrap1__main__menu__item">
-                        <a href="#!"><?php _e('О нас', 'ieb'); ?></a>
-                    </li>
-                </ul>
-            </nav>
-        </div> <!-- / .ieb-nav-wrap1 --> <!-- / .ieb-nav-half -->
-
-        <div class="ieb-nav-wrap2 ieb-nav-half">
-            <nav class="ieb-nav-wrap2__mobile">
-                <a href="#!" class="ieb-nav-wrap2__mobile__ham">
-                    <img src="http://demo.virtuti.info/demonstrations/vt12/wp-content/themes/pub/images/menu.png" alt="" class="ieb-nav-wrap2__mobile__ham__img">
-                </a>
-                <ul class="ieb-nav-wrap2__menu">
-                    <li class="ieb-nav-wrap2__menu__item">
-                        <a href="#!"><?php _e('Главная', 'ieb'); ?></a>
-                    </li>
-                    <li class="ieb-nav-wrap2__menu__item">
-                        <a href="#!"><?php _e('О нас', 'ieb'); ?></a>
-                    </li>
-                </ul>
-            </nav>
-        </div> <!-- / .ieb-mobile-wrap2 --> <!-- / .ieb-nav-half -->
+        <?php get_template_part('inc/front/navmenu'); ?>
 
         <div class="ieb-wrapper__content ieb-wrapper__content--in">
             <header class="ieb-header">
                 <figure class="ieb-header__figure">
-                    <img src="http://demo.virtuti.info/demonstrations/vt12/wp-content/uploads/2016/02/scene.jpg" alt="">
+                    <img src="<?php ieb_get_img('scene.jpg'); ?>"
+                        alt="<?php _e('Баннер изображение', 'ieb'); ?>"
+                    >
                 </figure>
             </header> <!-- / .ieb-header -->
         </div> <!-- / .ieb-wrapper__content -->
