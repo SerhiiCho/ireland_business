267e54db5f54bfc4ef52b1eb2020e96e460ccb99
SerhiiCho <serhiicho@inbox.ru>
2019-02-09 17:38:32 +0200

    -1- Added contact section with one text input and submit btn

---
+++ wp/src/wp-content/themes/ireland_business/inc/front/contact.php
@@ -0,0 +1,20 @@
+<div class="ieb-contact">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium maxime cum exercitationem delectus, placeat et laboriosam adipisci voluptatum veniam tenetur animi itaque ullam rem harum velit dolor possimus ut non!</p>
+
+    <div class="ieb-contact__inputs">
+        <div class="ieb-contact__inputs__wrap">
+            <label for="ieb-input-name"><?php _e('Имя', 'ieb'); ?></label>
+            <input type="text"
+                name="name" 
+                id="ieb-input-name"
+                class="ieb-contact__inputs__wrap__text"
+                placeholder="<?php _e('Имя', 'ieb'); ?>"
+            >
+        </div> <!-- / .ieb-contact__inputs__wrap -->
+        <div class="ieb-contact__inputs__wrap">
+            <button type="submit" class="ieb-contact__inputs__wrap__submit">
+                <?php _e('Отправить', 'ieb'); ?>
+            </button>
+        </div> <!-- / .ieb-contact__inputs__wrap -->
+    </div> <!-- / .ieb-contact__inputs -->
+</div> <!-- / .ieb-contact -->
