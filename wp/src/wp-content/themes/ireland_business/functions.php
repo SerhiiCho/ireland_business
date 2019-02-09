0a11faaa4a64b2603cf56e944512296cb19a34a6
SerhiiCho <serhiicho@inbox.ru>
2019-02-09 18:35:56 +0200

    -1- Added  custom post type called ieb-contact

--- wp/src/wp-content/themes/ireland_business/functions.php
+++ wp/src/wp-content/themes/ireland_business/functions.php
@@ -5,4 +5,5 @@ require_once get_template_directory() . '/inc/helpers.php';
 ieb_require_inc_files([
     'after-theme-support',
     'enqueue_scripts',
+    'custom-post-type',
 ]);
---
+++ wp/src/wp-content/themes/ireland_business/inc/custom-post-type.php
@@ -0,0 +1,21 @@
+<?php
+
+add_action('init', function () {
+    register_post_type('ieb-contact', [
+        'labels' => [
+            'name' => __('Сообщения', 'ieb'),
+            'singular_name' => __('Сообщение', 'ieb'),
+            'menu_name' => __('Сообщения', 'ieb'),
+            'name_admin_bar' => __('Сообщение', 'ieb'),
+        ],
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'menu_position' => 5,
+        'public' => true,
+        'menu_icon' => 'dashicons-email-alt',
+        'has_archive' => true,
+        'supports' => ['title', 'editor', 'author'],
+    ]);
+});
