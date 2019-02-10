6c470afa863af2e3b8be1b4eb89027f9839d9ffe
SerhiiCho <serhiicho@inbox.ru>
2019-02-10 15:37:22 +0200

    -1- Added wp_localize_script to enqueue_scripts.php file

--- wp/src/wp-content/themes/ireland_business/inc/enqueue_scripts.php
+++ wp/src/wp-content/themes/ireland_business/inc/enqueue_scripts.php
@@ -2,5 +2,12 @@
 
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('ieb-style', ieb_get_dir_uri('style.css'), [], ieb_file_ver('style.css'));
+    wp_register_script('ieb-js', ieb_get_dir_uri('assets/main.js'));
+
+    wp_localize_script('ieb-js', 'ieb_globals', [
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'nonce' => wp_create_nonce('ieb'),
+    ]);
+
     wp_enqueue_script('ieb-js', ieb_get_dir_uri('assets/main.js'), [], ieb_file_ver('assets/main.js'), true);
 });
