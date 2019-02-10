b00fc3ea1533880800b02eb73fd91f1432c3493b
SerhiiCho <serhiicho@inbox.ru>
2019-02-10 17:13:48 +0200

    -1- In columns.php changed the name of the column from 'name' to 'title'

--- wp/src/wp-content/themes/ireland_business/inc/post-type/columns.php
+++ wp/src/wp-content/themes/ireland_business/inc/post-type/columns.php
@@ -2,7 +2,7 @@
 
 add_filter('manage_ieb-contact_posts_columns', function($columns) {
     $new_columns = [
-        'name' => __('Имя', 'ieb'),
+        'title' => __('Имя', 'ieb'),
         'gender' => __('Пол', 'ieb'),
         'date' => __('Дата', 'ieb'),
     ];
@@ -11,7 +11,7 @@ add_filter('manage_ieb-contact_posts_columns', function($columns) {
 
 add_action('manage_ieb-contact_posts_custom_column', function($column, $post_id) {
     switch($column) {
-        case 'name':
+        case 'title':
             the_title();
             break;
         case 'gender':
