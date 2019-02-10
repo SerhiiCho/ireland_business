7f95132194d0621e063d0c469252c17bfe9c64ea
SerhiiCho <serhiicho@inbox.ru>
2019-02-10 11:44:39 +0200

    -1- Renamed css classes for contact form

--- wp/src/wp-content/themes/ireland_business/inc/front/contact.php
+++ wp/src/wp-content/themes/ireland_business/inc/front/contact.php
@@ -1,30 +1,32 @@
 <div class="ieb-contact">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium maxime cum exercitationem delectus, placeat et laboriosam adipisci voluptatum veniam tenetur animi itaque ullam rem harum velit dolor possimus ut non!</p>
 
-    <div class="ieb-contact__inputs">
-        <div class="ieb-contact__inputs__wrap">
+    <form method="post" class="ieb-contact__form">
+        <div class="ieb-contact__form__field">
             <label for="ieb-input-name"><?php _e('Имя', 'ieb'); ?></label>
             <input type="text"
                 name="name" 
                 id="ieb-input-name"
-                class="ieb-contact__inputs__wrap__text"
+                class="ieb-contact__form__field__text"
                 placeholder="<?php _e('Имя', 'ieb'); ?>"
             >
-        </div> <!-- / .ieb-contact__inputs__wrap -->
-        <div class="ieb-contact__inputs__wrap">
+        </div> <!-- / .ieb-contact__form__field -->
+
+        <div class="ieb-contact__form__field">
             <label for="ieb-input-gender"><?php _e('Пол', 'ieb'); ?></label>
             <select name="gender" 
                 id="ieb-input-gender"
-                class="ieb-contact__inputs__wrap__select"
+                class="ieb-contact__form__field__select"
             >
                 <option value="men"><?php _e('Мужской', 'ieb'); ?></option>
                 <option value="women"><?php _e('Женский', 'ieb'); ?></option>
             </select>
-        </div> <!-- / .ieb-contact__inputs__wrap -->
-        <div class="ieb-contact__inputs__wrap">
-            <button type="submit" class="ieb-contact__inputs__wrap__submit">
+        </div> <!-- / .ieb-contact__form__field -->
+
+        <div class="ieb-contact__form__field">
+            <button type="submit" class="ieb-contact__form__field__submit">
                 <?php _e('Отправить', 'ieb'); ?>
             </button>
-        </div> <!-- / .ieb-contact__inputs__wrap -->
-    </div> <!-- / .ieb-contact__inputs -->
+        </div> <!-- / .ieb-contact__form__field -->
+    </form> <!-- / .ieb-contact__form -->
 </div> <!-- / .ieb-contact -->
